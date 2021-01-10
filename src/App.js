2916a60d0a67b886906efcd71d22ae016a8aa24d
astribli <andrewjstribling@gmail.com>
2021-01-10 14:19:48 -0600

    broken setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'about me',
+      description: 'A little bit of info about me.',
+    },
+    { name: 'contact', description: 'A way to get in touch with me' },
+    { name: 'portfolio', description: 'A collection of projects I have done' },
+    { name: 'resume', description: 'View my current resume' },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
+      <main>
+        <div>
+       <About></About>
+        </div>
+      </main>
     </div>
   );
 }
 
 export default App;
+
---
+++ src/components/Nav/index.js
@@ -0,0 +1,47 @@
+
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import React, { useEffect } from 'react';
+
+function Nav(props) {
+  const {
+    categories = [],
+    setCurrentCategory,
+    currentCategory,
+  } = props;
+
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentCategory.name);
+  }, [currentCategory]);
+
+  return (
+    <header className="flex-row px-1">
+      <h2>
+        <a data-testid="link" href="/">
+          <span role="img" aria-label="camera"></span> AJ Stribling
+        </a>
+      </h2>
+      <nav>
+        <ul className="flex-row">
+          {categories.map((category) => (
+            <li
+              className={`mx-1 ${
+                currentCategory.name === category.name && 'navActive'
+                }`}
+              key={category.name}
+            >
+              <span
+                onClick={() => {
+                  setCurrentCategory(category)
+                }}
+              >
+                {capitalizeFirstLetter(category.name)}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Nav;
