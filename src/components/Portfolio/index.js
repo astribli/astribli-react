90c1bd1ada07b9f2f7fc164bae67f63262cb5d45
astribli <andrewjstribling@gmail.com>
2021-01-15 21:31:28 -0600

    update portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,37 +1,45 @@
 import React from 'react';
+import gamer from '../../assets/cover/GrubbyLogo.png'
+import run from '../../assets/cover/run-buddy.png'
+import meeting from '../../assets/cover/digital-marketing-meeting.jpg'
 
 function Portfolio() {
     return (
         <section id="portfolio" >
             <h2>Portfolio </h2>
             <div class="work-grid-container">
-                <a href="https://random-group-name.github.io/SubMyFood/" class="Sub-My-Food grid-item">
+                <a href="https://project-sticky-bandits.github.io/The-Grubby-Gamer/" class="Sub-My-Food grid-item">
                     <div>
-                        <h3>Sub My Food</h3>
-                        <span>HTML/CSS/JavaScript/APIs</span>
+                        <h3>The Grubby Gamer</h3>
+                        <img src={gamer} className="" style={{ width: "25%", maxHeight: "350px" }} alt="gamer" />
                     </div>
+                    <span>HTML/CSS/JavaScript/APIs</span>
+
                 </a>
 
-                <a href="https://merayoussef.github.io/Project-Run-Buddy/" class="Run-Buddy grid-item">
-                    <div>
-                        <h3>Run Buddy</h3>
+                <div>
+                    <a href="https://astribli.github.io/run-buddy/" class="Run-Buddy grid-item">
+                        <div>
+                            <h3>Run Buddy</h3>
+                            <img src={run} className="" style={{ width: "25%", maxHeight: "350px" }} alt="gamer" />
+                        </div>
+                        <div>
+                            <span>HTML/CSS</span>
+                        </div>
+                    </a>
+                </div>
+
+                <div>
+                    <a href="https://astribli.github.io/horiseon-challenge1/" class="Horiseon grid-item">
+                        <div>
+                            <h3>Horiseon</h3>
+                            <img src={meeting} className="" style={{ width: "25%", maxHeight: "350px" }} alt="people working" />
+                        </div>
+                        <div>
                         <span>HTML/CSS</span>
-                    </div>
-                </a>
-
-                <a href="https://merayoussef.github.io/Project-Horiseon/" class="Horiseon grid-item">
-                    <div>
-                        <h3>Horiseon</h3>
-                        <span>HTML/CSS</span>
-                    </div>
-                </a>
-
-                <a href="https://sma-to-success.herokuapp.com/" class="Winter-Wishlist grid-item">
-                    <div>
-                        <h3>Winter Wishlist</h3>
-                        <span>Handlebars/JavaScript/Node</span>
-                    </div>
-                </a>
+                        </div>
+                    </a>
+                </div>
             </div>
         </section>
     );
