72ff888e9ebb6c8e06ea0649587092bae409fcf3
astribli <andrewjstribling@gmail.com>
2021-01-15 20:59:33 -0600

    Redo components and get working

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
+import ContactForm from './components/Contact';
+import Portfolio from './components/Portfolio';
+import coverImage from '../src/assets/cover/Mullet.jpg'
+
 
 function App() {
   const [categories] = useState([
     {
-      name: 'about me',
-      description: 'A little bit of info about me.',
-    },
-    { name: 'contact', description: 'A way to get in touch with me' },
-    { name: 'portfolio', description: 'A collection of projects I have done' },
-    { name: 'resume', description: 'View my current resume' },
+      name: 'Portfolio',
+      description: "photos and project links to my work",
+    }
   ]);
 
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
@@ -21,15 +24,22 @@ function App() {
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
       ></Nav>
+      <img src={coverImage} className="" style={{ width: "25%", maxHeight: "350px" }} alt="cover" />
       <main>
-        <div>
-       <About></About>
-        </div>
+        {!contactSelected ? (
+          <>
+            <Portfolio currentCategory={currentCategory}></Portfolio>
+            <About></About>
+          </>
+        ) : (
+            <ContactForm></ContactForm>
+          )}
       </main>
     </div>
   );
 }
 
 export default App;
-
--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
-import coverImage from "../../assets/cover/AJ.jpg";
 
 function About() {
-    return (
-        <section className="my-5">
-            <h1 id="about">Who am I?</h1>
-            <img src={coverImage} className="my-2" style={{ width: "25%" }} alt="cover" />
-        </section>
-    );
-}
 
+  return (
+    <section className="my-5" >
+      <h1 id="about">About Me</h1>
+      <div className="my-2">
+        <p>
+        My name is AJ Stribling and I was born and raised in Crawfordsville, IN. By day you will see me as a Leasing Specialist in property management. By night you may see me with a camera in my hand. I am passionate about the arts and want to put that creativity into web development. Building a career in something I am passionate about is very important to me. From film and photography to coding, I will strive for greatness.
+                </p>
+        <p>
+          I am currently learning about HTML, CSS, Bootstrap, Java, JQuery and APIs. I practice coding daily and I am excited to learn all the main concepts to become a full-stack developer.
+                </p>
+      </div>
+    </section>
+  )
+}
 export default About;
