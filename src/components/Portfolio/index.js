b46de885dea24172763a1f4a0a6f523361bcf575
astribli <andrewjstribling@gmail.com>
2021-02-07 18:25:53 -0600

    Add to portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import gamer from '../../assets/cover/GrubbyLogo.png'
 import run from '../../assets/cover/run-buddy.png'
 import meeting from '../../assets/cover/digital-marketing-meeting.jpg'
+import tourney from '../../assets/cover/tourney-tracker.png'
+import parents from '../../assets/cover/landingpage.PNG'
 
 function Portfolio() {
     return (
@@ -37,6 +39,28 @@ function Portfolio() {
                         </div>
                         <div>
                         <span>HTML/CSS</span>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="https://powerful-citadel-84342.herokuapp.com/" class="Tee-Tracker grid-item">
+                        <div>
+                            <h3>Horiseon</h3>
+                            <img src={tourney-tracker} className="" style={{ width: "25%", maxHeight: "350px" }} alt="people working" />
+                        </div>
+                        <div>
+                        <span>HTML/CSS/JavaScript/APIs</span>
+                        </div>
+                    </a>
+                </div>
+                <div>
+                    <a href="https://arcane-reaches-47656.herokuapp.com/" class="Parents-only grid-item">
+                        <div>
+                            <h3>Horiseon</h3>
+                            <img src={parents} className="" style={{ width: "25%", maxHeight: "350px" }} alt="people working" />
+                        </div>
+                        <div>
+                        <span>HTML/CSS/JavaScript/APIs</span>
                         </div>
                     </a>
                 </div>
